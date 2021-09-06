1fbf8aa9d1d65f017e67b4d24f554a1529357476
chenkui <jihe12180@gmail.com>
2021-09-06 22:11:46 +0800

    first commit

---
+++ src/main/java/com/perfma/xlab/xpocket/plugin/pstack/PStackXPocketCommand.java
@@ -0,0 +1,11 @@
+package com.perfma.xlab.xpocket.plugin.pstack;
+
+import com.perfma.xlab.xpocket.spi.command.AbstractXPocketCommand;
+
+/**
+ * @author jihe
+ */
+public class PStackXPocketCommand extends AbstractXPocketCommand {
+
+
+}
---
+++ src/main/java/com/perfma/xlab/xpocket/plugin/pstack/PStackXPocketPlugin.java
@@ -0,0 +1,10 @@
+package com.perfma.xlab.xpocket.plugin.pstack;
+
+import com.perfma.xlab.xpocket.spi.AbstractXPocketPlugin;
+
+/**
+ * @author jihe
+ */
+public class PStackXPocketPlugin extends AbstractXPocketPlugin {
+
+}
